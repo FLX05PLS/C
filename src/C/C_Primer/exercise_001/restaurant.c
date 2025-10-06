8f6d4719a9931fc388b1a3442dbac10081a1f1da
FLX05PLS <1242755081@qq.com>
2025-10-07 00:36:28 +0800

    Update restaurant.c

--- src/C/C_Primer/exercise_001/restaurant.c
+++ src/C/C_Primer/exercise_001/restaurant.c
@@ -20,44 +20,59 @@ int main(void)
     int people_number;//用餐人数
     int id_dishes;//菜品序号
 
-    printf("请输入会员编号：");
-    scanf("%d", &id_number);
+    int all_price;
+    //点单用的
 
-    if (id_number == 2013456)
+    do
     {
-        printf("欢迎光临！%d\n", id_number);
+        printf("请输入会员编号：");
+        scanf("%d", &id_number);
+        if (id_number == 2013456)
+        {
+            printf("欢迎光临！%d\n", id_number);
 
-        printf("早餐时间：8:00-10:00\n");
-        printf("A1 鸡蛋 20元\n");
-        printf("A2 奶酪 10元\n");
-        printf("A3 沙拉 15元\n");
-        printf("A4 牛肉汤 25元\n");
+            printf("早餐时间:8:00-10:00\n");
+            printf("A1 鸡蛋 20元\n");
+            printf("A2 奶酪 10元\n");
+            printf("A3 沙拉 15元\n");
+            printf("A4 牛肉汤 25元\n");
 
-        printf("午餐时间：11:00-13:00\n");
-        printf("B1 牛肉烧烤 20元\n");
-        printf("B2 猪肉烧烤 20元\n");
-        printf("B3 鸡肉烧烤 15元\n");
-        printf("B4 烧烤牛肉汉堡 25元\n");
+            printf("午餐时间:11:00-13:00\n");
+            printf("B1 牛肉烧烤 20元\n");
+            printf("B2 猪肉烧烤 20元\n");
+            printf("B3 鸡肉烧烤 15元\n");
+            printf("B4 烧烤牛肉汉堡 25元\n");
 
-        printf("晚餐时间：17:00-19:00\n");
-        printf("C1 猪肉烧烤 20元\n");
-        printf("C2 蔬菜猪肉烧烤 10元\n");
-        printf("C3 猪排烧烤 15元\n");
-        printf("C4 猪肉串 25元\n");
-
-        printf("请输入用餐人数：");
-        scanf("%d", &people_number);
-        printf("用餐人数：%d\n", people_number);//这里需要塞一个循环，输入一个正整数执行下一步，否则提示重新输入
-        
-        printf("请输入菜品序号：");
-        scanf("%d", &id_dishes);
-        printf("已购买%d，需要加菜吗？", id_dishes);//循环
+            printf("晚餐时间:17:00-19:00\n");
+            printf("C1 猪肉烧烤 20元\n");
+            printf("C2 蔬菜猪肉烧烤 10元\n");
+            printf("C3 猪排烧烤 15元\n");
+            printf("C4 猪肉串 25元\n");//菜单展示
 
 
-    }
+            printf("请输入用餐人数：");
+            scanf("%d", &people_number);
+            printf("用餐人数：%d\n", people_number);
 
-    else
-        printf("对不起，您不是会员！请输入会员编号：");//这里需要塞一个循环
+            printf("请输入菜品序号：");
+            scanf("%d", &id_dishes);
+            printf("已购买%d,需要加菜吗?", id_dishes);//循环
+            //x=x+y
+
+
+
+
+            break;
+        }
+        else
+        {
+            printf("对不起，您不是会员！\n");
+        }
+    } while (1);//循环整个点单系统
     
     return 0;
 }
+//1.使用id登录系统，展示菜单
+//2.输入用餐人数，用户进行点单
+//3.结束点单循环，获取点单结果并计算总价
+//4.打印小票
